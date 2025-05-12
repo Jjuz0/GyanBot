47fd81fe89d05508e80a4beda5a761976106549b
Jjuz0 <aaproo140@gmail.com>
2025-05-12 19:07:57 +0300

    Create first files

---
+++ app.py
@@ -0,0 +1,17 @@
+import disnake
+import os
+from disnake.ext import commands
+from config import token
+
+intents = disnake.Intents.default()
+intents.members = True
+bot = commands.Bot(intents=intents)
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+
+
+
+bot.run(token)
+
---
+++ config.py
@@ -0,0 +1,1 @@
+token = '' #Write Token Bot
